c71e0d89cb34349fa94c0261c7ef76c1135665b7
francosorbello <franco.sorbelo@gmail.com>
2026-02-27 20:57:37 -0300

    added poppins font

--- where_is_my_pal/settings.py
+++ where_is_my_pal/settings.py
@@ -116,4 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',   # tells Django to look in the project-level static folder
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # where collectstatic outputs to
