07a550d12e0229ee173b345524c1961fa4f40861
francosorbello <franco.sorbelo@gmail.com>
2026-02-28 12:26:40 -0300

    added bootstrap. Added starting point for pet detail page

--- wimp_base/views/pet_views.py
+++ wimp_base/views/pet_views.py
@@ -1,4 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from wimp_base.models.pet_model import Pet
+from wimp_base.models.owner_model import Owner
 
 def pet_detail(request, pet_id):
-    return HttpResponse("You're looking at pet %s." % pet_id)
+    pet = None
+    try:
+        pet = Pet.objects.get(pk=pet_id)
+        owner = pet.owner
+
+        context = {
+            "pet_name" : pet.name,
+            "owner_name" : owner.name,
+            "owner_phone" : owner.phone_number
+        }
+    except Pet.DoesNotExist:
+        raise Http404(f"No pet with id ${pet_id}")
+    except Owner.DoesNotExist:
+        if pet != None:
+            raise Http404(f"${pet.name} desnt have an owner")
+        else:
+            raise Http404("Invalid pet and/or owner")
+    
+    return render(request,"wimp_base/pet_detail.html",context)
