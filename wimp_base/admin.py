1af3a69f936f8b307794a2931fa76704e39fbd05
francosorbello <franco.sorbelo@gmail.com>
2026-03-02 16:53:18 -0300

    added system to crop pet photos when uploading them

--- wimp_base/admin.py
+++ wimp_base/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from wimp_base.models.owner_model import OwnerAdmin
+from wimp_base.models.pet_model import PetProfileAdmin
 from wimp_base.models import Pet, Owner
 
 # Register your models here.
-admin.site.register(Pet)
+admin.site.register(Pet, PetProfileAdmin)
 admin.site.register(Owner, OwnerAdmin)
--- wimp_base/models/pet_model.py
+++ wimp_base/models/pet_model.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django import forms
 from .owner_model import Owner
+from django.contrib import admin
+import base64
+from django.core.files.base import ContentFile
 
 class Pet(models.Model):
     name = models.CharField("Pet Name", max_length=50)
@@ -7,3 +11,22 @@ class Pet(models.Model):
     photo = models.ImageField(upload_to="pets/",default='pets/default.jpg')
     def __str__(self) -> str:
         return self.name
+    
+class PetProfileAdminForm(forms.ModelForm):
+    # cropped image is hidden, as it only holds the data for the cropped photo
+    cropped_image = forms.CharField(widget=forms.HiddenInput(), required=False)
+    class Meta:
+        model = Pet
+        fields = '__all__'
+
+class PetProfileAdmin(admin.ModelAdmin):
+    form = PetProfileAdminForm
+
+    # when saving model, replace the uploaded picture with a cropped version
+    def save_model(self, request, obj, form, change):
+        cropped_data = form.cleaned_data.get('cropped_image')
+        if cropped_data:
+            format, imgstr = cropped_data.split(';base64,')
+            ext = format.split('/')[-1] # image type (eg: jpg, png, etc)
+            obj.photo = ContentFile(base64.b64decode(imgstr), name=f'pet_pic.{ext}')
+        super().save_model(request, obj, form, change)
