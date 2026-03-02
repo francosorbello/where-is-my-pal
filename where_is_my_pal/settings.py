818c9f9b6909223c4ba93a5d73b5c2c1e8198c81
francosorbello <franco.sorbelo@gmail.com>
2026-03-02 15:52:02 -0300

    added image field to pet model

--- where_is_my_pal/settings.py
+++ where_is_my_pal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +124,5 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # where collectstatic outputs to
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
--- wimp_base/models/pet_model.py
+++ wimp_base/models/pet_model.py
@@ -4,5 +4,6 @@ from .owner_model import Owner
 class Pet(models.Model):
     name = models.CharField("Pet Name", max_length=50)
     owner = models.ForeignKey(Owner,on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="pets/",default='pets/default.jpg')
     def __str__(self) -> str:
         return self.name
