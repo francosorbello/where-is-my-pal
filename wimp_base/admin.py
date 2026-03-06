2fd6216ecd2cfe2508d3438d88b7f0328e3c9a96
francosorbello <franco.sorbelo@gmail.com>
2026-03-06 13:46:08 -0300

    Admins can create qr codes that redirect to pets

--- wimp_base/admin.py
+++ wimp_base/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from wimp_base.models.owner_model import OwnerAdmin
 from wimp_base.models.pet_model import PetProfileAdmin
-from wimp_base.models import Pet, Owner, SiteSettings
+from wimp_base.models import Pet, Owner, SiteSettings, QrPet
 
-# Register your models here.
+
 admin.site.register(Pet, PetProfileAdmin)
 admin.site.register(Owner, OwnerAdmin)
 
@@ -17,3 +18,39 @@ class SiteSettingsAdmin(admin.ModelAdmin):
 
 
 admin.site.register(SiteSettings, SiteSettingsAdmin)
+
+
+@admin.register(QrPet)
+class QrPetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pet', 'redirect_to', 'qr_thumbnail')
+    list_filter = ('pet',)
+    search_fields = ('pet__name', 'redirect_to')
+    readonly_fields = ('id', 'redirect_to', 'qr_image_preview', 'qr_image')
+    ordering = ('-pk',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('pet',)
+        }),
+        ('QR Code', {
+            'fields': ('redirect_to', 'qr_image_preview', 'qr_image')
+        }),
+    )
+
+    def qr_thumbnail(self, obj):
+        if obj.qr_image:
+            return format_html('<img src="{}" width="50" height="50" />', obj.qr_image.url)
+        return '-'
+    qr_thumbnail.short_description = 'QR'
+
+    def qr_image_preview(self, obj):
+        if obj.qr_image:
+            return format_html('<img src="{}" width="150" height="150" />', obj.qr_image.url)
+        return '-'
+    qr_image_preview.short_description = 'QR Code Preview'
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if obj.qr_image:
+            obj.generate_qr_code()
+            obj.save()
--- wimp_base/models/qr_model.py
+++ wimp_base/models/qr_model.py
@@ -1,8 +1,83 @@
 import uuid
+import io
 from django.db import models
+from django.conf import settings
+import qrcode
+
 
 class QrPet(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
-    redirect_to = models.CharField("Redirects to",max_length=255,blank=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    pet = models.ForeignKey(
+        'Pet', 
+        on_delete=models.CASCADE, 
+        related_name='qr_codes',
+        null=True,
+        blank=True,
+        help_text="Pet to generate QR code for"
+    )
+    redirect_to = models.CharField(
+        "Redirects to", 
+        max_length=255, 
+        blank=True,
+        help_text="Auto-generated URL based on pet ID"
+    )
+    qr_image = models.ImageField(
+        "QR Code Image", 
+        upload_to='qr_codes/', 
+        blank=True,
+        help_text="Generated QR code image"
+    )
+
     def __str__(self) -> str:
-        return "Redirects to:"+self.redirect_to
+        return f"QR for {self.pet.name}"
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            try:
+                old_instance = QrPet.objects.get(pk=self.pk)
+                old_pet_id = old_instance.pet.id if old_instance.pet else None
+            except QrPet.DoesNotExist:
+                old_pet_id = None
+        else:
+            old_pet_id = None
+        
+        new_pet_id = self.pet.id if self.pet else None
+        pet_changed = old_pet_id != new_pet_id
+        
+        if self.pet:
+            self.redirect_to = f"/pets/{self.pet.id}/"
+        
+        super().save(*args, **kwargs)
+        
+        if self.pet and (pet_changed or not self.qr_image):
+            self.generate_qr_code()
+
+    def generate_qr_code(self):
+        if not self.pet or not self.redirect_to:
+            return
+            
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        
+        full_url = self.redirect_to
+        if not full_url.startswith('http'):
+            full_url = settings.ALLOWED_HOSTS[0] + self.redirect_to if settings.ALLOWED_HOSTS else 'localhost' + self.redirect_to
+        
+        qr.add_data(full_url)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        
+        buffer = io.BytesIO()
+        img.save(buffer, format='PNG')
+        buffer.seek(0)
+        
+        filename = f"qr_{self.pet.id}_{self.id}.png"
+        self.qr_image.save(filename, buffer)
+        buffer.close()
+        
+        super().save(update_fields=['qr_image'])
